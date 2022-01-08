ffaad921685f3b5eb667d25561fb01c61d505150
AlexxBelous <connectionwithme1@gmail.com>
2022-01-08 08:27:42 +0200

    commit

---
+++ app/js/main.js
@@ -0,0 +1,12 @@
+$(function(){
+
+$('.header__slider').slick({
+    infinite: true,
+    fade: true,
+    prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt="/img">',
+    nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt="/img">'
+
+  
+});
+
+// end 50 min 45 sek
